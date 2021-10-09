7b19292a157ddf517a04d2fc2e7546d295a00f1d
Charliechzzz <89336345+Charliechzzz@users.noreply.github.com>
2021-10-09 17:04:08 +0800

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,4 @@
+var vg_1 = "js/chart.json";
+vegaEmbed('#chart', vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
